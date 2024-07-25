d4fe1d0377a1a9c69d1fd0605ae0a1982724e8ef
lehariya <lehariyabanu@gmail.com>
2024-07-25 10:40:40 +0530

    TestCommit

---
+++ src/Navigation.js
@@ -0,0 +1,11 @@
+import{ Link } from "react";
+function Navigation(){
+    return(
+        <div>
+        <Link to="/">Home</Link>
+        <Link to="/profile" >profile</Link>
+        </div>
+    );
+
+}
+export default Navigation;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM  from "react-dom/client";
-import App from "./App";
+import Navigation from "./Navigation";
 //import One from "./one"
 //import Ten from "./ten"
 const rootElement = document.getElementById('root')
 const rootRef = ReactDOM.createRoot(rootElement);
-rootRef.render(<App/>);
+rootRef.render(<Navigation/>);
